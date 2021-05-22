aff64b28355ea60d8494ca4c5840713e72cf776b
Yoshihiro Tanaka <contact@cordea.jp>
2021-05-22 13:27:39 +0900

    find added files

--- bin/gendarttest.dart
+++ bin/gendarttest.dart
@@ -1,3 +1,22 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  _run();
+}
+
+Future<void> _run() async {
+  final files = await _findAddedFiles();
+}
+
+Future<Iterable<String>> _findAddedFiles() async {
+  final result = await Process.run(
+    'git',
+    ['ls-files', '-o', '--exclude-standard'],
+  );
+  if (result.exitCode > 0) {
+    throw Exception(result.stderr);
+  }
+  return (result.stdout as String)
+      .split('\n')
+      .where((element) => element.endsWith('.dart'));
 }
